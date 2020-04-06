efa4b38d1e37c7f8888d0d1f38d1047453bd4d49
josephmwells <joester3@gmail.com>
2020-04-06 15:57:39 -0700

    implemented rotation and flipping transformations

--- effects.cpp
+++ effects.cpp
@@ -103,9 +103,9 @@ void blur(Bitmap & b)
 
   for(int y = 0; y < b.height(); ++y) {
     for(int x = 0; x < b.width(); ++x) {
-      uint32_t r_new{0};
-      uint32_t g_new{0};
-      uint32_t b_new{0};
+      uint32_t r_blur{0};
+      uint32_t g_blur{0};
+      uint32_t b_blur{0};
       // apply blur to pixels in a 5x5 block
       for(int i = -2; i <= 2; ++i) {
         for(int j = -2; j <= 2; ++j) {
@@ -117,18 +117,18 @@ void blur(Bitmap & b)
           if(x+j >= b.width() || y+i >= b.height()) {
             continue;
           }
-          r_new += b.r(x+j, y+i) * gause_matrix.at((i+2) * 5 + (j+2)); 
-          g_new += b.g(x+j, y+i) * gause_matrix.at((i+2) * 5 + (j+2)); 
-          b_new += b.b(x+j, y+i) * gause_matrix.at((i+2) * 5 + (j+2)); 
+          r_blur += b.r(x+j, y+i) * gause_matrix.at((i+2) * 5 + (j+2)); 
+          g_blur += b.g(x+j, y+i) * gause_matrix.at((i+2) * 5 + (j+2)); 
+          b_blur += b.b(x+j, y+i) * gause_matrix.at((i+2) * 5 + (j+2)); 
         }
       }
-      r_new /= 256;
-      g_new /= 256;
-      b_new /= 256;
+      r_blur /= 256;
+      g_blur /= 256;
+      b_blur /= 256;
 
-      b.r(x, y) = r_new;
-      b.g(x, y) = g_new;
-      b.b(x, y) = b_new;
+      b.r(x, y) = r_blur;
+      b.g(x, y) = g_blur;
+      b.b(x, y) = b_blur;
     }
   }
   
@@ -136,30 +136,157 @@ void blur(Bitmap & b)
 
 void rot90(Bitmap & b)
 {
+  uint32_t padding = 4 - (3 * b.height()) % 4;
+  std::vector<unsigned char> rotate;
+
+  for(int x = b.width() - 1; x >= 0; --x) {
+    for(int y = 0; y < b.height(); ++y) {
+      if(b.depth() == 32)
+        rotate.push_back(b.a(x, y));
+      rotate.push_back(b.b(x, y)); 
+      rotate.push_back(b.g(x, y));
+      rotate.push_back(b.r(x, y));
+    }
+    if(b.depth() == 24)
+      for (uint32_t i = 0; i < padding; ++i)
+        rotate.push_back(0);
+  }
+
+  b.image() = rotate;
+  int32_t width = b.width();
+  int32_t height = b.height();
+  b.width() = height;
+  b.height() = width;
 }
 
 void rot180(Bitmap & b)
 {
+  std::vector<unsigned char> rotate;
+
+  for(int y = b.height() - 1; y >= 0; --y) {
+    for(int x = b.width() - 1; x >= 0; --x) {
+      if(b.depth() == 32)
+        rotate.push_back(b.a(x, y));
+      rotate.push_back(b.b(x, y)); 
+      rotate.push_back(b.g(x, y));
+      rotate.push_back(b.r(x, y));
+    }
+    if(b.depth() == 24)
+      for (uint32_t i = 0; i < b.padding(); ++i)
+        rotate.push_back(0);
+  }
+
+  b.image() = rotate;
 }
 
 void rot270(Bitmap & b)
 {
+  uint32_t padding = 4 - (3 * b.height()) % 4;
+  std::vector<unsigned char> rotate;
+
+  for(int x = 0; x < b.width(); ++x) {
+    for(int y = b.height() - 1; y >= 0; --y) {
+      if(b.depth() == 32)
+        rotate.push_back(b.a(x, y));
+      rotate.push_back(b.b(x, y)); 
+      rotate.push_back(b.g(x, y));
+      rotate.push_back(b.r(x, y));
+    }
+    if(b.depth() == 24)
+      for (uint32_t i = 0; i < padding; ++i)
+        rotate.push_back(0);
+  }
+
+  b.image() = rotate;
+  int32_t width = b.width();
+  int32_t height = b.height();
+  b.width() = height;
+  b.height() = width;
 }
 
 void flipv(Bitmap & b)
 {
+  std::vector<unsigned char> flipped;
+
+  for (int y = b.height() - 1; y >= 0; --y) {
+    for(int x = 0; x < b.width(); ++x) {
+      if(b.depth() == 32)
+        flipped.push_back(b.a(x, y));
+      flipped.push_back(b.b(x, y));
+      flipped.push_back(b.g(x, y));
+      flipped.push_back(b.r(x, y));
+    }
+    for(uint32_t i = 0; i < b.padding(); i++)
+      flipped.push_back(0);
+  }
+  b.image() = flipped;
 }
 
 void fliph(Bitmap & b)
 {
+  std::vector<unsigned char> flipped;
+
+  for (int y = 0; y < b.height(); ++y) {
+    for(int x = b.width() - 1; x >= 0; --x) {
+      if(b.depth() == 32)
+        flipped.push_back(b.a(x, y));
+      flipped.push_back(b.b(x, y));
+      flipped.push_back(b.g(x, y));
+      flipped.push_back(b.r(x, y));
+    }
+    if(b.depth() == 24)
+      for(uint32_t i = 0; i < b.padding(); i++)
+        flipped.push_back(0);
+  }
+  b.image() = flipped;
 }
 
 void flipd1(Bitmap & b)
 {
+  uint32_t padding = 4 - (3 * b.height()) % 4;
+  std::vector<unsigned char> flipped;
+
+  for(int x = b.width() -1; x >= 0; --x) {
+    for (int y = b.height() - 1; y >= 0; --y) {
+      if(b.depth() == 32)
+        flipped.push_back(b.a(x, y));
+      flipped.push_back(b.b(x, y));
+      flipped.push_back(b.g(x, y));
+      flipped.push_back(b.r(x, y));
+    }
+    if(b.depth() == 24)
+      for(uint32_t i = 0; i < padding; i++)
+        flipped.push_back(0);
+  }
+  b.image() = flipped;
+  int32_t width = b.width();
+  int32_t height = b.height();
+  b.width() = height;
+  b.height() = width;
 }
 
 void flipd2(Bitmap & b)
 {
+  uint32_t padding = 4 - (3 * b.height()) % 4;
+  std::vector<unsigned char> flipped;
+
+  for(int x = 0; x < b.width(); ++x) {
+    for (int y = 0; y < b.height(); ++y) {
+      if(b.depth() == 32)
+        flipped.push_back(b.a(x, y));
+      flipped.push_back(b.b(x, y));
+      flipped.push_back(b.g(x, y));
+      flipped.push_back(b.r(x, y));
+    }
+    if(b.depth() == 24)
+      for(uint32_t i = 0; i < padding; i++)
+        flipped.push_back(0);
+  }
+  b.image() = flipped;
+  int32_t width = b.width();
+  int32_t height = b.height();
+  b.width() = height;
+  b.height() = width;
 }
 
 void scaleUp(Bitmap & b)
